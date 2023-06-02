22824cf5e204b392dca7c9e80362454c3547ab10
Mario <mariokpo.mb@gmail.com>
2023-06-02 17:37:54 -0300

    modificando el diseño

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
-// Button.js
 import React from 'react';
 import '../styles/Button.css';
 
 const Button = ({ onClick, label, id }) => {
+  const isOperator = ['+', '-', '*', '/', '='].includes(label);
+  const isClear = label === 'Clear';
+  const buttonClass = isOperator ? 'button operator' : isClear ? 'button clear' : 'button';
+
   return (
-    <button onClick={onClick} id={id} className="button">
+    <button onClick={onClick} id={id} className={buttonClass}>
       {label}
     </button>
   );
